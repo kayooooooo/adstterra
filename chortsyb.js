fb9f772e176ed339a35fe5b67ae0a877c00982a4
kayooooooo <108146877+kayooooooo@users.noreply.github.com>
2023-06-21 15:41:20 -0300

    Update chortsyb.js

--- chortsyb.js
+++ chortsyb.js
@@ -51,7 +51,7 @@ function buscarVideosShorts() {
 
         // Exibir o feed de vídeos curtos no Blogger
         // Exibir o feed de vídeos curtos no Blogger
-  		var meuFeedDeVideosCurto = document.getElementById('meu-feed-de-videos-curto');
+  		var meuFeedDeVideosCurto = document.querySelector('.meu-feed-de-videos-curto');
   		meuFeedDeVideosCurto.innerHTML = feedHtml;
 
   		// Iniciar o carrossel com os vídeos retornados pela API do YouTube
